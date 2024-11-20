79b1da0a35c8d9443b493a8dcceedaf7603a45fc
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-11-20 17:18:17 +0400

    S

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
+
 const dev = false //process.env.NODE_ENV !== "production";
-const app = next({ dev, dir: '.', conf: { distDir: 'build' }});
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass true as the second argument to url.parse.
+    // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true);
     const { pathname, query } = parsedUrl;
-    // Redirect /news to /News
-    if (pathname.includes( "/news/")) {
-      res.writeHead(301, { Location: pathname.replace('/news/',"/News/") });
-      res.end();
-      return;
-    }
 
     if (pathname === "/a") {
       app.render(req, res, "/a", query);
@@ -26,7 +21,7 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl);
     }
-  }).listen(2000, (err) => {
-    if (err) throw err;
-  });
+  }).listen(process.env.PORT || 3500, (err) => {
+    if (err) throw err;
+  });
 });
