1e65d8349286752f51d412b02b5b6624518da2fc
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-11-21 16:07:56 +0400

    S

--- src/components/widgets/Hero/index.js
+++ src/components/widgets/Hero/index.js
@@ -262,7 +262,7 @@ const Hero = (props) => {
             setInternalState({ [`${destination}-search-focus-${index}`]: window.innerWidth > 990 ? false : true })
             const container = document?.querySelector(`#content${index}${destination}`);
             e.target.style.opacity = 1
-            setTimeout(() => { window.scroll({ top: container?.offsetTop, left: 0, behavior: "smooth", }) }, 10);
+            setTimeout(() => { window.scroll({ top: 0, left: 0, behavior: "smooth", }) }, 10);
         }
     }
 
